47df79441dc827f422b2b0870c042b8a620d107b
João Paulo <joaowais@gmail.com>
2023-08-04 20:11:30 -0300

    finalizado alguns metodos de fs e path

--- modules/fs.js
+++ modules/fs.js
@@ -9,3 +9,39 @@ const path = require('path');
 //   }
 //   console.log('Pasta Criada com Sucesso');
 // });
+
+// Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, '/test', 'test.txt'),
+  'Hello Node!',
+  (error) => {
+    if (error) {
+      return console.log('Erro:', error);
+    }
+    console.log('Arquivo criado com sucesso!');
+  }
+);
+
+// Adicionar a um arquivo
+fs.appendFile(
+  path.join(__dirname, '/test', 'test.txt'),
+  'Hello World!',
+  (error) => {
+    if (error) {
+      return console.log('Erro:', error);
+    }
+    console.log('Arquivo modificado com sucesso');
+  }
+);
+
+// Ler um arquivo
+fs.readFile(
+  path.join(__dirname, '/test', 'test.txt'),
+  'utf-8',
+  (error, data) => {
+    if (error) {
+      console.log('Erro:', error);
+    }
+    console.log(data);
+  }
+);
